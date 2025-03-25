50b76f4711dfa9cec07f77b4782c080a63a51d9d
uranny <164876325+uranny@users.noreply.github.com>
2025-03-25 22:04:39 +0900

    feat : 무한 스크롤

--- component/banner/banner.tsx
+++ component/banner/banner.tsx
@@ -1,10 +1,81 @@
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, Text, StyleSheet, FlatList, Dimensions} from 'react-native';
+import React, {useRef, useState, useCallback} from "react";
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function Banner() {
+    const flatUseRef = useRef<FlatList>(null)
+    const normalLst = ["홈이당1", "홈이당2", "홈이당3", "홈이당4", "홈이당5", "홈이당1"];
+    const [index, setIndex] = useState(0);
+    const screenWidth = Dimensions.get('window').width - 32; // 현재 화면의 너비 가져오기
+    const slideSecond = () => {
+        if (index == normalLst.length-1){
+            return 0
+        }
+        return 2000
+    }
+
+    // last Index일때 처음으로 돌아가기
+    useFocusEffect(
+        useCallback(() => {
+            const interval = setInterval(() => {
+                console.log(`interval 들어옴, 현재 ${index}`);
+                setIndex((prevIndex) => {
+                    const nextIndex = (prevIndex + 1) % normalLst.length
+                    if(prevIndex == normalLst.length-1){
+                        flatUseRef.current?.scrollToIndex({ 
+                            index: nextIndex, 
+                            animated: false 
+                        })
+                    } else{
+                        flatUseRef.current?.scrollToIndex({ 
+                            index: nextIndex,
+                            animated: true 
+                        })
+                    }
+                    console.log(`from ${prevIndex} to ${nextIndex}`);
+                    return nextIndex
+                })
+            }, index === normalLst.length - 1 ? 0 : 2000)
+            return () => clearInterval(interval)
+        }, [])
+    );
+
     return (
         <SafeAreaView>
-            
+            <FlatList
+            style={styles.flatLst}
+            ref={flatUseRef}
+            data={normalLst}
+            horizontal
+            keyExtractor={(_, i) => (i.toString())}
+            showsHorizontalScrollIndicator={false}
+            scrollEnabled={false}
+            contentContainerStyle={{gap:20}}
+            renderItem={({item}) => (
+                <SafeAreaView style={[styles.view, {width : screenWidth}]}>
+                    <Text style={styles.text}>{item}</Text>
+                </SafeAreaView>
+            )}/>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    flatLst : {
+        marginTop : 24,
+        marginLeft : 16,
+        marginRight : 16,
+    },
+    view : {
+        height:200,
+        backgroundColor : 'rgb(255, 99, 99)',
+        borderColor : '#000000',
+        borderStyle : 'solid',
+        borderWidth : 1,
+        alignItems : 'center',
+        justifyContent : 'center',
+        borderRadius : 10
+    },
+    text : {
+    }
+})
--- component/tabs/home.tsx
+++ component/tabs/home.tsx
@@ -1,40 +1,12 @@
 import { SafeAreaView, Text, StyleSheet, FlatList, Dimensions} from 'react-native';
-import React, {useState, useEffect, useRef} from 'react';
-import Banner from '../banner/banner';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import SliderBanner from '../banner/banner';
 
 export default function HomeScreen(){
-    const flatUseRef = useRef<FlatList>(null)
-    const lst = ["홈이당1", "홈이당2", "홈이당3", "홈이당4", "홈이당5", "홈이당6", "홈이당7", "홈이당8", "홈이당9"];
-    const [index, setIndex] = useState(0);
-    const screenWidth = Dimensions.get('window').width - 32; // 현재 화면의 너비 가져오기
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIndex((prevIndex) => {
-                const nextIndex = (prevIndex + 1) % lst.length
-                flatUseRef.current?.scrollToIndex({ index: nextIndex, animated: true })
-                console.log(`인덱스 바뀜 ${nextIndex}`);
-                return nextIndex
-            })
-        }, 5000)
-        return () => clearInterval(interval)
-    }, [lst])
 
     return (
         <SafeAreaView style={styles.container}>
-            <FlatList
-            style={styles.flatLst}
-            ref={flatUseRef}
-            data={lst}
-            horizontal
-            keyExtractor={(_, i) => (i.toString())}
-            scrollEnabled={false}
-            renderItem={({item}) => (
-                <SafeAreaView style={[styles.view, {width : screenWidth}]}>
-                    <Text style={styles.text}>{item}</Text>
-                </SafeAreaView>
-            )}
-            showsHorizontalScrollIndicator={false}/>
+            <SliderBanner/>
         </SafeAreaView> 
     );
 }
@@ -43,22 +15,5 @@ const styles = StyleSheet.create({
     container : {
         flex : 1,
         backgroundColor: '#f5f5f5'
-    },
-    flatLst : {
-        marginTop : 24,
-        marginLeft : 16,
-        marginRight : 16,
-    },
-    view : {
-        height:200,
-        backgroundColor : 'rgb(255, 99, 99)',
-        borderColor : '#000000',
-        borderStyle : 'solid',
-        borderWidth : 1,
-        alignItems : 'center',
-        justifyContent : 'center',
-        borderRadius : 10
-    },
-    text : {
     }
 })
